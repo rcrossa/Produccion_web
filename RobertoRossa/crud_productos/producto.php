e0628f1b8021b04fc503ff28dcac764933b67bf8
Roberto Rossa <rcrossa@hotmail.com>
2020-09-16 18:58:44 -0500

    Update producto.php

--- RobertoRossa/crud_productos/producto.php
+++ RobertoRossa/crud_productos/producto.php
@@ -13,23 +13,23 @@ class producto {
 
     function __construct(){}
     public function getId(){ return $this->id ;}
-    public function setId($id){return $this->id = $id;}
+    public function setId($id){ $this->id = $id;}
     public function getContinente(){ return $this->continente ;}
-    public function setContinente($continente){return $this->continente = $continente;}
+    public function setContinente($continente){ $this->continente = $continente;}
     public function getPais(){return $this->pais ;}
-    public function setPais($pais){return $this->pais = $pais;}
+    public function setPais($pais){ $this->pais = $pais;}
     public function getPrecio(){return $this->precio ;}
-    public function setPrecio($precio){return $this->precio = $precio;}
+    public function setPrecio($precio){ $this->precio = $precio;}
     public function getDescripcion(){return $this->pais ;}
-    public function setDescripcion($descripcion){return $this->$descripcion = $descripcion;}
+    public function setDescripcion($descripcion){ $this->$descripcion = $descripcion;}
     public function getDescripcion_details(){return $this->descripcion_details ;}
-    public function setDescripcion_details($descripcion_details){return $this->descripcion_details = $descripcion_details;}
+    public function setDescripcion_details($descripcion_details){ $this->descripcion_details = $descripcion_details;}
     public function getUrl(){return $this->url ;}
-    public function setUrl($url){return $this->url = $url;}
+    public function setUrl($url){ $this->url = $url;}
     public function getDestacado(){return $this->destacado ;}
-    public function setDestacado($destacado){return $this->destacado = $destacado; }
+    public function setDestacado($destacado){ $this->destacado = $destacado; }
     public function getActivo(){return $this->activo ;}
-    public function setActivo($activo){return $this->activo = $activo;}
+    public function setActivo($activo){ $this->activo = $activo;}
 }
 
 ?>
