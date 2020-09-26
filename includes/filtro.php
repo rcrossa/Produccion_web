b0deb85dd9feda9ef1d7b6f80e98686aebead623
Roberto Rossa <rcrossa@hotmail.com>
2020-09-26 17:18:16 -0300

    update rama test

--- includes/filtro.php
+++ includes/filtro.php
@@ -1,105 +1,140 @@
 <?php
-$str_data_continentes = file_get_contents("./json/continentes.json");
-$str_data_paises = file_get_contents("./json/paises.json");
-$str_data_estadoprovincia = file_get_contents("./json/estadosprovincias.json");
+//API conversión MySQL a JSON
+require_once ('conexion.php');
+    $db=DB::conectar();
+    
+    $queryContinente = "SELECT idcontinente as id, nombrecontinente as nombre FROM continentes WHERE activo=1";
+    $queryPais = "SELECT DISTINCT pa.idpais as id, pa.nombrepais as pais, co.nombrecontinente as continente  
+    FROM productos pr, ciudades ci, paises pa, continentes co
+    WHERE pr.idciudad = ci.idciudad 
+    AND ci.idpais = pa.idpais
+    AND pa.idcontinente = co.idcontinente 
+    AND pa.activo=1";
 
-$dataContinentes = json_decode($str_data_continentes, true);
-$dataPaises = json_decode($str_data_paises, true);
-$dataEstadosProvincias = json_decode($str_data_estadoprovincia, true);
+    $dataCiudad = "SELECT ci.nombreciudad as ciudades, pa.nombrepais as pais, co.nombrecontinente as continente
+    FROM productos pr, ciudades ci, paises pa, continentes co
+    WHERE pr.idciudad = ci.idciudad 
+    AND ci.idpais = pa.idpais
+    AND pa.idcontinente = co.idcontinente";
 
-//incluye la clase Producto y CrudProducto
-require_once ('crud_productos.php');
-require_once ('producto.php');
-$crud = new CrudProducto();
-$producto= new Producto();
-$listarProductos=$crud->mostrar();
+    $stmt = $db->prepare($queryContinente);
+    $stmt2 = $db->prepare($queryPais);
+    $stmt3 = $db->prepare($dataCiudad);
+    $stmt->execute();
+    $stmt2->execute();
+    $stmt3->execute();
+
+    $dataContinentes = array();
+    $dataPaises = array();
+    $dataCiudades = array();
+
+    while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+         $dataContinentes[] = $row;
+    }
+
+    while($row=$stmt2->fetch(PDO::FETCH_ASSOC)){
+        $dataPaises[] = $row;
+        }
+
+    while($row=$stmt3->fetch(PDO::FETCH_ASSOC)){
+        $dataCiudades[] = $row;
+    }
+    
+    
+ //Fin de API
+
+//$str_data_continentes = file_get_contents("./json/continentes.json");
+//$str_data_paises = file_get_contents("./json/paises.json");
+//$str_data_ciudades = file_get_contents("./json/ciudades.json");
+
+//$dataContinentes = json_decode($str_data_continentes, true);
+//$dataPaises = json_decode($str_data_paises, true);
+//$dataciudades = json_decode($str_data_ciudades, true);
+
 
 ?>
-<div>
-<table class="table table-bordered tablamostrar1 ">
-		<thead class="table-dark">
-            <th>Pais</th>
-		</thead>
-		<tbody>
-			<?php foreach ($listarProductos as $producto) {?>
-			<tr class="table-info">
-                <td><?php echo $producto->getIdProducto() ?></td>
-                <td><?php echo $producto->getPais() ?></td>
-                <td><?php echo $producto->getCiudad() ?></td>
-                <td><?php echo $producto->getPrecio() ?></td>
-                <td><?php echo $producto->getDescripcion() ?></td>
-                <td><?php echo $producto->getDetalle() ?></td>
-                <td><?php echo $producto->getUrl() ?></td>
-                <td><?php echo $producto->getDestacado() ?></td>
-                <td><?php echo $producto->getActivo() ?></td>
-			</tr>
-			<?php }?>
-		</tbody>
-	</table>
-</div>
+
 <div class="container pt-4 px-5">
     <div class="row justify-content-center">
         <div class="col-12">
             <div class="filter-wrap py-4">
-                <h3>Filtrar</h3>
+                <h3>Filtro</h3>
                 <div class="filter-border p-4 border border-secondary">
                     <div class="filter-inner">
                         <div class="filtrolugar">
                             <div class="row justify-content-center align-items-center">
                                 <div class="col-12 col-md-6 col-lg-4 py-2">
                                     <form action="" method="GET" class="">
+
+                                        <select class="custom-select custom-select-sm" name="orden">
+                                        <option value="" selected></option>
+                                            <option value="asc">Ordenar Ascendente</option>
+                                            <option value="desc">Ordenar Descendente</option>
+                                        </select>
+                              
                                         <?php $opcionContinen = 'Todo'; ?>
                                         <?php !empty($_GET['continente']) ? $opcionContinen = $_GET['continente'] : $opcionContinen = "" ?>
-                                        <select name="continente" class="custom-select custom-select-lg" id="continente" onchange="this.form.submit()">
+                                        <select name="continente" class="custom-select custom-select-lg" id="continente"
+                                            onchange="this.form.submit()">
                                             <option value="" selected="selected">Seleccionar Continente</option>
                                             <?php foreach ($dataContinentes as $continentes) : ?>
-                                                <option <?php echo ($opcionContinen == $continentes['nombre']) ? 'selected="selected"' : '' ?>>
-                                                    <?php echo $continentes['nombre'] ?>
-                                                </option>
+                                            <option
+                                                <?php echo ($opcionContinen == $continentes['nombre']) ? 'selected="selected"' : '' ?>>
+                                                <?php echo $continentes['nombre'] ?>
+                                            </option>
                                             <?php endforeach ?>
                                         </select>
                                     </form>
                                 </div>
                                 <div class="col-10 col-md-6 col-lg-4">
                                     <form action="" method="GET" class="">
+                                    <select class="custom-select custom-select-sm" name="orden">
+                                        <option value="" selected></option>
+                                            <option value="asc">Ordenar Ascendente</option>
+                                            <option value="desc">Ordenar Descendente</option>
+                                        </select>
                                         <?php $opcionPais = 'Todo'; ?>
                                         <?php !empty($_GET['pais']) ? $opcionPais=$_GET['pais']: $opcionPais= "Todo" ?>
-                                         <input type="hidden" name="continente" value="<?php echo isset($opcionContinen) ? $opcionContinen  : $_GET['continente']?>">
-                                        <select name="pais" class="custom-select custom-select-lg" id="pais" onchange="this.form.submit()">
+                                        <input type="hidden" name="continente"
+                                            value="<?php echo isset($opcionContinen) ? $opcionContinen  : $_GET['continente']?>">
+                                        <select name="pais" class="custom-select custom-select-lg" id="pais"
+                                            onchange="this.form.submit()">
                                             <option value="">Seleccionar Pais</option>
                                             <?php foreach ($dataPaises as $paises) : ?>
-                                                <?php if ($paises['continente'] == $opcionContinen) : ?>
-                                                    <option <?php echo ($opcionPais == $paises['pais']) ? 'selected="selected"' : ''?>>
-                                                    <?php echo $paises['pais'];?> </option>
-                                                <?php endif ?>
-                                                <?php if ($_GET['continente'] == null || $_GET['continente'] == 'Todo') : ?>
-                                                    <option <?php echo ($opcionPais == $paises['pais']) ? 'selected="selected"' : ''?>>
-                                                    <?php echo $paises['pais'];?> </option>
-                                                <?php endif ?>
-
+                                            <?php if ($paises['continente'] == $opcionContinen) : ?>
+                                            <option
+                                                <?php echo ($opcionPais == $paises['pais']) ? 'selected="selected"' : ''?>>
+                                                <?php echo $paises['pais'];?> </option>
+                                            <?php endif ?>
+                                            <?php if ($_GET['continente'] == null || $_GET['continente'] == 'Todo') : ?>
+                                            <option
+                                                <?php echo ($opcionPais == $paises['pais']) ? 'selected="selected"' : ''?>>
+                                                <?php echo $paises['pais'];?> </option>
+                                            <?php endif ?>
                                             <?php endforeach ?>
                                         </select>
                                     </form>
                                 </div>
-                                <div  class="col-12 col-md-6 col-lg-4">
-                                <form action="" method="GET" class="">
-                                <?php $opcionEstado = 'Todo'; ?>
-
-                                <input type="hidden" name="continente" value="<?php echo isset($opcionContinen) ? $opcionContinen  : $_GET['continente']?>">
-                                <input type="hidden" name="pais" value="<?php echo isset($opcionPais) ? $opcionPais : $_GET['pais'] ?>">
-                                   
-
-                                    <?php !empty($_GET['estadoprovincia']) ? $opcionEstado=$_GET['estadoprovincia']: $opcionEstado = "Todo" ?>
-                                        <select name="estadoprovincia" class="custom-select custom-select-lg" id="estadoprovincia" onchange="this.form.submit()">
-                                            <option value="">Estados / Provincias</option>
-                                            <?php foreach ($dataEstadosProvincias as $estadosprovincias) : ?>
-                                                <?php if ($estadosprovincias["pais"] == $opcionPais) : ?>
-                                                    <option <?php echo ($opcionEstado == $estadosprovincias['estadoprovincia']) ? 'selected="selected"' : ''?>>
-                                                    <?php echo $estadosprovincias['estadoprovincia'];?> </option>
-                                                <?php endif ?>
+                                <div class="col-12 col-md-6 col-lg-4">
+                                    <form action="" method="GET" class="">
+                                        <?php $opcionCiudad = 'Todo'; ?>
+                                        <input type="hidden" name="continente"
+                                            value="<?php echo isset($opcionContinen) ? $opcionContinen  : $_GET['continente']?>">
+                                        <input type="hidden" name="pais"
+                                            value="<?php echo isset($opcionPais) ? $opcionPais : $_GET['pais'] ?>">
+                                        <?php !empty($_GET['ciudades']) ? $opcionCiudad=$_GET['ciudades']: $opcionCiudad = "Todo" ?>
+                                        <select name="ciudades" class="custom-select custom-select-lg" id="ciudades"
+                                            onchange="this.form.submit()">
+                                            <option value="">Seleccionar Ciudad</option>
+                                            <?php foreach ($dataCiudades as $ciudades) : ?>
+                                            <?php if ($ciudades["pais"] == $opcionPais) : ?>
+                                            <option
+                                                <?php echo ($opcionCiudad == $ciudades['ciudades']) ? 'selected="selected"' : ''?>>
+                                                <?php echo $ciudades['ciudades'];?> </option>
+                                            <?php endif ?>
                                             <?php endforeach ?>
-                                        </select> 
-                                        </form>   
+                                        </select>
+                                    </form>
                                 </div>
                             </div>
                         </div>
@@ -110,71 +145,3 @@ $listarProductos=$crud->mostrar();
 
     </div>
 </div>
-
-<script>
-    // $(document).ready(function() {
-
-    //     load_json_data('continente');
-
-    //     function load_json_data(id, parent_id) {
-    //         var html_code = '';
-    //         $.getJSON('./json/ciudades.json', function(data) {
-
-    //             html_code += '<option value="">Select ' + id + '</option>';
-    //             $.each(data, function(key, value) {
-    //                 if (id == 'continente') {
-    //                     if (value.continente == '0') {
-    //                         html_code += '<option value="' + value.continente + '">' + value.continenteNombre + '</option>';
-    //                     } else {
-    //                         html_code += '<option value="' + value.continente + '">' + value.continenteNombre + '</option>';
-    //                     }
-    //                 } else if (id == 'pais') {
-
-    //                     if (value.continente == parent_id ) {
-    //                         html_code += '<option value="' + value.pais + '">' + value.paisNombre + '</option>';
-    //                     }
-
-    //                 } else {
-    //                     if (value.pais == parent_id) {
-    //                         html_code += '<option value="' + value.id + '">' + value.nombre + '</option>';
-    //                     }
-    //                 }
-    //             });
-    //             $('#' + id).html(html_code);
-    //             norepeat();
-    //         });
-
-    //     }
-
-    //     $(document).on('change', '#continente', function() {
-    //         var continente_id = $(this).val();
-    //         if (continente_id != '') {
-    //             load_json_data('pais', continente_id);
-    //         } else {
-    //             load_json_data('pais', continente_id);
-    //         }
-    //     });
-
-    //     $(document).on('change', '#pais', function() {
-    //         var pais_id = $(this).val();
-    //         if (pais_id != '') {
-    //             load_json_data('ciudad', pais_id);
-    //         } else {
-    //             load_json_data('ciudad', pais_id);
-    //         }
-    //     });
-
-    //     function norepeat() {
-    //         var seen = {};
-    //         $('option').each(function() {
-    //             var txt = $(this).text();
-    //             if (seen[txt]) {
-    //                 $(this).remove();
-    //             } else {
-    //                 seen[txt] = true;
-    //             }
-    //         });
-    //     }
-
-    // });
-</script>
--- includes/productoportada.php
+++ includes/productoportada.php
@@ -5,33 +5,117 @@
       <div class="row">
 
         <?php
+      //API conversión MySQL a JSON
+        require_once ('conexion.php');
+        $db=DB::conectar();
+       
+        $campo="nombre";
+        $orden="";
+        $ORDERBY = "ORDER BY";
+
+        $queryProductos = "SELECT pr.idproducto as id, 
+                            ci.nombreciudad as nombre, 
+                            pa.nombrepais as pais, 
+                            co.nombrecontinente as continente,
+                            ci.nombreciudad as ciudades,
+                            pr.precio as precio,
+                            pr.descripcion as descripcion,
+                            pr.detalle as descripcion_details,
+                            pr.url as url,
+                            pr.destacado as destacado
+                            FROM productos pr, ciudades ci, paises pa, continentes co
+                            WHERE pr.idciudad = ci.idciudad 
+                            AND ci.idpais = pa.idpais
+                            AND pa.idcontinente = co.idcontinente 
+                            AND pr.activo=1
+                            $ORDERBY $campo $orden";
+        
+
+
+        $stmt4 = $db->prepare($queryProductos);
+        $stmt4->execute();
+        $dataProductos = array();
+
+
+        while($row=$stmt4->fetch(PDO::FETCH_ASSOC)){
+            $dataProductos[] = $row;
+        }
+        //Fin del código
+
         $continente = (isset($_GET["continente"]) ? $_GET['continente'] : null);
         $pais = (isset($_GET["pais"]) ? $_GET['pais'] : null);
-        $estadosprovincias = (isset($_GET["estadoprovincia"]) ? $_GET['estadoprovincia'] : null)
-
+        $ciudades = (isset($_GET["ciudades"]) ? $_GET['ciudades'] : null)
 
         ?>
-        <?php foreach ( $productos as $key => $value) { ?>
+        <?php foreach ( $dataProductos as $key => $value) { ?>
          
           <?php if ($page == 'index' && $value['destacado']) { ?>
 
             <?php include('card_paises.php'); ?>
 
-          <?php } elseif ($page == 'catalogo') { ?>
+          <?php }?>
+          <?php } ?>
+
+
+
+          <!-- ---Select catalogo -->
+          <?php
+          if ($page == 'catalogo') { ?>
 
             <?php
-            $condicionEstado = (($continente == $value['continente'] || $continente == 'Todo') && $pais == $value['pais'] && $estadosprovincias == $value['estadoprovincia']);
-            $condicionPais = ($pais == $value['pais'] && empty($estadosprovincias));
-            $condicionContinente = (($continente == $value['continente'] || $continente == 'Todo') && empty($pais) && empty($estadosprovincias ));
+            // $ordenamiento = (isset($_GET["orden"])? $_GET['orden']: null);
+               $campo="nombre";
+               $orden=(isset($_GET["orden"])? $_GET['orden']: null);
+               $ORDERBY = "ORDER BY";
+       
+               $queryProductos1 = "SELECT pr.idproducto as id, 
+                                   ci.nombreciudad as nombre, 
+                                   pa.nombrepais as pais, 
+                                   co.nombrecontinente as continente,
+                                   ci.nombreciudad as ciudades,
+                                   pr.precio as precio,
+                                   pr.descripcion as descripcion,
+                                   pr.detalle as descripcion_details,
+                                   pr.url as url,
+                                   pr.destacado as destacado
+                                   FROM productos pr, ciudades ci, paises pa, continentes co
+                                   WHERE pr.idciudad = ci.idciudad 
+                                   AND ci.idpais = pa.idpais
+                                   AND pa.idcontinente = co.idcontinente 
+                                   AND pr.activo=1
+                                   $ORDERBY $campo $orden";
+               
+       
+       
+               $stmt6 = $db->prepare($queryProductos1);
+               $stmt6->execute();
+               $dataProductos1 = array();
+       
+       
+               while($row=$stmt6->fetch(PDO::FETCH_ASSOC)){
+                   $dataProductos1[] = $row;
+               }
+               //Fin del código
+               
+               $continente = (isset($_GET["continente"]) ? $_GET['continente'] : null);
+               $pais = (isset($_GET["pais"]) ? $_GET['pais'] : null);
+               $ciudades = (isset($_GET["ciudades"]) ? $_GET['ciudades'] : null)
+            ?>
+            <?php foreach ( $dataProductos1 as $key => $value) { ?>
+          
+            <?php
+            $condicionCiudad = (($continente == $value['continente'] || $continente == 'Todo') && $pais == $value['pais'] && $ciudades == $value['ciudades']);
+            $condicionPais = ($pais == $value['pais'] && empty($ciudades));
+            $condicionContinente = (($continente == $value['continente'] || $continente == 'Todo') && empty($pais) && empty($ciudades ));
   
-            if ($condicionEstado || $condicionPais || $condicionContinente) {
+            if ($condicionCiudad || $condicionPais || $condicionContinente) {
             ?>
               <?php include('card_paises.php'); ?>
 
-            <?php } ?>
+            
           <?php } ?>
         <?php } ?>
-
+        <?php } ?>
       </div>
     </div>
 
