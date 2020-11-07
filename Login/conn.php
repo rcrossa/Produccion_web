f2ed2cc761b74a0802753880bdcd75e2c1964882
Roberto Rossa <rcrossa@hotmail.com>
2020-11-07 16:52:56 -0300

    update

--- Login/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Connection variables
-$dbhost	= "localhost";	    // localhost or IP
-$dbuser	= "root1";		    // database username
-$dbpass	= "1234";		     // database password
-$dbname	= "produccion_web5";    // database name
-?>
